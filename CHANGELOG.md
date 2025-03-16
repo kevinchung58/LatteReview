8ed088118a138caf99195d553130ed6327a26a6c
Pouria Rouzrokh <po.rouzrokh@gmail.com>
2025-03-16 19:02:24 -0400

    updated to v1.0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Writing the white paper for the package and public launch
 
+## [1.0.5] - 2025-3-16
+
+### Fixed
+
+- Fixed initalization of the `lattereview.utils.data_handler` module.
+
 ## [1.0.4] - 2025-3-16
 
 ### Added
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="lattereview",
-    version="1.0.4",
+    version="1.0.5",
     author="Pouria Rouzrokh",
     author_email="po.rouzrokh@gmail.com",
     description="A framework for multi-agent review workflows using large language models",
